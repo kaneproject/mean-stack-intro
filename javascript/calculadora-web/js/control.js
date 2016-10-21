51a3a544d6a4073e3503d0d029adae6518f2d18d
Profesormanana <Profesormanana>
2016-10-21 09:58:23 +0200

    Primera aproximacion calculadora

---
+++ javascript/calculadora-web/js/control.js
@@ -0,0 +1,12 @@
+var memoria;
+calculadora = new Calculadora();
+function agregarAlDisplay(valor){
+    //TODO: Tengo que ingresar en el input el valor que me llega
+}
+function operacion(operador){
+    //TODO; en caso de que me llegue un +
+        //TODO: Almacenarlo en la memoria
+    //TODO: en caso de que me llegue un igual
+        // TODO: llamar a la calculadora con el valor de memoria, el valor del display 
+        calculadora.sumar(memoria,document.getElementById("display"));
+}
