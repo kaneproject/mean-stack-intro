8b82d18d2ec82adcd3e34b3177050fb6dbc9bd6d
Profesormanana <Profesormanana>
2016-11-29 12:57:29 +0100

    ejemplo async y schema mongoose

---
+++ javascript/ejemplo09-mongoose/ejemplo02schema.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
+// definicion del esquema
+var Schema = mongoose.Schema;
+var LibroSchema = new Schema({
+    titulo:     String,
+    autor:      String,
+    sinopsis:   String,
+    fecha:      Date,
+    categoria:  String,
+    campos_biblioteca:{
+        ejemplares: Number,  
+        reservas:   Number,
+        ultima_reserva: Date
+    }
+})
+var Libro = mongoose.model("Libro",LibroSchema);
+var lotr = new Libro();/*
+    {
+        titulo:"Lord of the rings",
+        campos_biblioteca:{
+            ultima_reserva: new Date()
+        }
+    });*/
+// Guardo:
+lotr.save((error)=>{
+    if(error){
+        console.error("Error al guardar: ",error);
+    }else{
+        console.log("Salvado con id: " + lotr._id);
+        Libro.find((error,data)=>{
+            console.log(data);
+        });
+    }
+})
+// Listo:
+Libro.find((error,data)=>{
+    console.log(data);
+});
