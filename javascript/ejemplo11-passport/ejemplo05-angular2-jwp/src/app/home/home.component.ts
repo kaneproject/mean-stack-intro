c7022b5fbc870fe4065215ec3e587d7524bba187
Profesormanana <Profesormanana>
2016-12-14 12:52:17 +0100

    Fin ejemplo jwt

--- javascript/ejemplo11-passport/ejemplo05-angular2-jwp/src/app/home/home.component.ts
+++ javascript/ejemplo11-passport/ejemplo05-angular2-jwp/src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PassportService } from '../passport.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [PassportService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: PassportService) { }
 
   ngOnInit() {
+    this.service.getMemberInfo().subscribe((data)=>{
+      console.log(data);
+    })
   }
 
 }
